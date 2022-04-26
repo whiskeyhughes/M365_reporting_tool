20b1a0f0e223f1167997228640ee2496ef7c956a
Joshua Hughes <joshua.hughesfl@gmail.com>
2022-04-26 12:14:52 -0400

    added column compare row per Matt A

--- licenses.py
+++ licenses.py
@@ -18,6 +18,8 @@ def defineVariables():
     col1, col2, col3 = st.columns(3)
     global m365_standard_bundles
     m365_standard_bundles = st.sidebar.selectbox('Base License', ["None", "Microsoft 365 Business Apps", "Microsoft 365 Business Basic", "Microsoft 365 Business Standard", "Microsoft 365 Business Premium", "Microsoft 365 Enterprise Apps", "Microsoft 365 Enterprise F1", "Microsoft 365 Enterprise F3", "Microsoft 365 Enterprise E3", "Microsoft 365 Enterprise E3 + (E5 Sec)", "Microsoft 365 Enterprise E3 + (E5 Comp)", "Microsoft 365 Enterprise E5", "Office 365 Exchange Online Plan 1", "Office 365 Exchange Online Plan 2", "Office 365 Enterprise F3", "Office 365 Enterprise E1", "Office 365 Enterprise E3", "Office 365 Enterprise E5"])
+    global m365_standard_bundles_compare
+    m365_standard_bundles_compare = st.sidebar.selectbox('Base License - Compare', ["None", "Microsoft 365 Business Apps", "Microsoft 365 Business Basic", "Microsoft 365 Business Standard", "Microsoft 365 Business Premium", "Microsoft 365 Enterprise Apps", "Microsoft 365 Enterprise F1", "Microsoft 365 Enterprise F3", "Microsoft 365 Enterprise E3", "Microsoft 365 Enterprise E3 + (E5 Sec)", "Microsoft 365 Enterprise E3 + (E5 Comp)", "Microsoft 365 Enterprise E5", "Office 365 Exchange Online Plan 1", "Office 365 Exchange Online Plan 2", "Office 365 Enterprise F3", "Office 365 Enterprise E1", "Office 365 Enterprise E3", "Office 365 Enterprise E5"])
     global m365_ems_bundles
     m365_ems_bundles = st.sidebar.selectbox('EMS Bundle', ["None", "Enterprise Mobility + Security E3", "Enterprise Mobility + Security E5"])
     global m365_mdo_bundles 
@@ -60,20 +62,20 @@ def defineVariables():
             security_weight_total += points    
 
     if security_weight_total == 1:
-        col2.write("🍆")
-        col2.caption("That is one of the smallest Security Eggplants we have ever seen...")
+        st.sidebar.write("🍆")
+        st.sidebar.caption("That is one of the smallest Security Eggplants we have ever seen...")
     elif security_weight_total == 2:
-        col2.subheader("🍆")
-        col2.caption("That is one average size Security Eggplant...")
+        st.sidebar.subheader("🍆")
+        st.sidebar.caption("That is one average size Security Eggplant...")
     elif security_weight_total == 3:
-        col2.header("🍆")
-        col2.caption("Wow that is a very large Security Eggplant!!!")
+        st.sidebar.header("🍆")
+        st.sidebar.caption("Wow that is a very large Security Eggplant!!!")
     elif security_weight_total == 4:
-        col2.header("🍆")
-        col2.caption("Wow that is a very large Security Eggplant!!!")
+        st.sidebar.header("🍆")
+        st.sidebar.caption("Wow that is a very large Security Eggplant!!!")
     elif security_weight_total >= 5:
-        col2.title("🍆💦")
-        col2.caption("THAT IS THE BIGGEST SECURITY EGGPLANT WE HAVE EVER SEEN!!!!!")
+        st.sidebar.title("🍆💦")
+        st.sidebar.caption("THAT IS THE BIGGEST SECURITY EGGPLANT WE HAVE EVER SEEN!!!!!")
     
 
 
@@ -367,6 +369,253 @@ def featuresProvidedBasedOnChosenBaseBundle():
                 col1.table(w10_plan_table_style)
                 col1.table(addon_plan_table_style)
                 col1.table(aad_plan_table_style)
+##### Function for the Base License Dropdown #####
+def featuresProvidedBasedOnChosenBaseBundleColumn2():
+    for m365_base_service in m365_base_services:
+        for feature, license in m365_base_services[m365_base_service].items():   
+            if license == m365_standard_bundles_compare:
+                
+                ##### Filter Dict based on M365 Base License Chosen by user #####
+                base_result = m365_base_services[m365_base_service]
+                base_dict = { key:value for (key,value) in base_result.items() if value != "No"}
+                
+                ##### Add the EMS Dict based on M365 Base License Chosen by user #####
+                ems_result = m365_ems_services[m365_base_service]
+                ems_dict = { key:value for (key,value) in ems_result.items() if value != "No" if key != "EMS"}
+                
+                ##### Add the MDO Dict based on M365 Base License Chosen by user #####
+                mdo_result = m365_mdo_services[m365_base_service]
+                mdo_dict = { key:value for (key,value) in mdo_result.items() if value != "No" if key != "Defender for Office"}
+                
+                ##### Add the VoIP Dict based on M365 Base License Chosen by user #####
+                voip_result = m365_voip_services[m365_base_service]
+                voip_dict = { key:value for (key,value) in voip_result.items() if value != "No" if key != "VoIP"}
+                
+                ##### Add the W10 Dict based on M365 Base License Chosen by user #####
+                w10_result = m365_w10_services[m365_base_service]
+                w10_dict = { key:value for (key,value) in w10_result.items() if value != "No" if key != "Windows 10"}
+                
+                ##### Add the Addon Feature Dict based on M365 Base License Chosen by user #####
+                addon_result = m365_addon_services[m365_base_service]
+                addon_dict = { key:value for (key,value) in addon_result.items() if value != "No" if key != "Add-Ons"}
+
+                # ##### Add the Addon Feature Dict based on M365 Base License Chosen by user #####
+                aad_result = m365_aad_services[m365_base_service]
+                aad_dict = { key:value for (key,value) in aad_result.items() if value != "No" if key != "Azure AD Features"} 
+                
+                ##### Convert the Dicts to a Pandas Dataframe and Clean Up Indexes and Columns #####
+                base_plan_table_design = pd.DataFrame.from_dict(base_dict, orient="Index", columns=[f'Included in {license}']).reset_index().rename(columns = {'index':'Base Services'})
+                base_egg_plant_list = []
+                base_egg_plant_count = 0
+                while base_egg_plant_count < len(base_plan_table_design):
+                    base_egg_plant_list.append("🍆")
+                    base_egg_plant_count += 1
+                base_plan_table_design['💦'] = base_egg_plant_list
+                base_plan_table_design.set_index('💦', inplace=True)
+ 
+                ems_plan_table_design = pd.DataFrame.from_dict(ems_dict, orient="Index", columns=[f'Included in {license}']).reset_index().rename(columns = {'index':'Enterprise Mobility & Security'})
+                ems_egg_plant_list = []
+                ems_egg_plant_count = 0    
+                while ems_egg_plant_count < len(ems_plan_table_design):
+                    ems_egg_plant_list.append("🍆")
+                    ems_egg_plant_count += 1
+                ems_plan_table_design['💦'] = ems_egg_plant_list
+                ems_plan_table_design.set_index('💦', inplace=True)
+
+                mdo_plan_table_design = pd.DataFrame.from_dict(mdo_dict, orient="Index", columns=[f'Included in {license}']).reset_index().rename(columns = {'index':'Microsoft Defender Online'})
+                mdo_egg_plant_list = []
+                mdo_egg_plant_count = 0    
+                while mdo_egg_plant_count < len(mdo_plan_table_design):
+                    mdo_egg_plant_list.append("🍆")
+                    mdo_egg_plant_count += 1
+                mdo_plan_table_design['💦'] = mdo_egg_plant_list
+                mdo_plan_table_design.set_index('💦', inplace=True)
+
+                voip_plan_table_design = pd.DataFrame.from_dict(voip_dict, orient="Index", columns=[f'Included in {license}']).reset_index().rename(columns = {'index':'VoIP'})
+                voip_egg_plant_list = []
+                voip_egg_plant_count = 0    
+                while voip_egg_plant_count < len(voip_plan_table_design):
+                    voip_egg_plant_list.append("🍆")
+                    voip_egg_plant_count += 1
+                voip_plan_table_design['💦'] = voip_egg_plant_list
+                voip_plan_table_design.set_index('💦', inplace=True)
+
+                w10_plan_table_design = pd.DataFrame.from_dict(w10_dict, orient="Index", columns=[f'Included in {license}']).reset_index().rename(columns = {'index':'Windows 10'})
+                w10_egg_plant_list = []
+                w10_egg_plant_count = 0    
+                while w10_egg_plant_count < len(w10_plan_table_design):
+                    w10_egg_plant_list.append("🍆")
+                    w10_egg_plant_count += 1
+                w10_plan_table_design['💦'] = w10_egg_plant_list
+                w10_plan_table_design.set_index('💦', inplace=True)
+
+                addon_plan_table_design = pd.DataFrame.from_dict(addon_dict, orient="Index", columns=[f'Included in {license}']).reset_index().rename(columns = {'index':'Misc. Addons'})
+                addon_egg_plant_list = []
+                addon_egg_plant_count = 0    
+                while addon_egg_plant_count < len(addon_plan_table_design):
+                    addon_egg_plant_list.append("🍆")
+                    addon_egg_plant_count += 1
+                addon_plan_table_design['💦'] = addon_egg_plant_list
+                addon_plan_table_design.set_index('💦', inplace=True)
+
+                aad_plan_table_design = pd.DataFrame.from_dict(aad_dict, orient="Index", columns=[f'Included in {license}']).reset_index().rename(columns = {'index':'Azure AD Features'})
+                aad_egg_plant_list = []
+                aad_egg_plant_count = 0    
+                while aad_egg_plant_count < len(aad_plan_table_design):
+                    aad_egg_plant_list.append("🍆")
+                    aad_egg_plant_count += 1
+                aad_plan_table_design['💦'] = aad_egg_plant_list
+                aad_plan_table_design.set_index('💦', inplace=True)
+
+                ##### Add Pandas Styles to the Dataframe #####
+                base_plan_table_style =  base_plan_table_design.style.set_table_styles([
+                            {
+                                "selector":"thead",
+                                "props": [("background-color", "LightCoral"), ("color", "white"),
+                                          ("border", "3px solid white"),
+                                          ("font-size", "1rem"), ("font-weight:", "bold")]
+                            },
+                            {
+                                "selector":"th.row_heading",
+                                "props": [("background-color", "SteelBlue"), ("color", "white"),
+                                          ("border", "3px solid white"),
+                                          ("font-size", "1rem"), ("font-style", "italic")]
+                            },
+                            {
+                                "selector":"th.col_heading",
+                                "props": [("color", "white"),
+                                          ("border", "3px solid white"),
+                                          ("font-size", "1rem"), ("font-weight:", "bold")]
+                            }
+                        ])
+                ems_plan_table_style =  ems_plan_table_design.style.set_table_styles([
+                            {
+                                "selector":"thead",
+                                "props": [("background-color", "LightCoral"), ("color", "white"),
+                                          ("border", "3px solid white"),
+                                          ("font-size", "1rem"), ("font-weight:", "bold")]
+                            },
+                            {
+                                "selector":"th.row_heading",
+                                "props": [("background-color", "SteelBlue"), ("color", "white"),
+                                          ("border", "3px solid white"),
+                                          ("font-size", "1rem"), ("font-style", "italic")]
+                            },
+                            {
+                                "selector":"th.col_heading",
+                                "props": [("color", "white"),
+                                          ("border", "3px solid white"),
+                                          ("font-size", "1rem"), ("font-weight:", "bold")]
+                            }
+                        ])
+                mdo_plan_table_style =  mdo_plan_table_design.style.set_table_styles([
+                            {
+                                "selector":"thead",
+                                "props": [("background-color", "LightCoral"), ("color", "white"),
+                                          ("border", "3px solid white"),
+                                          ("font-size", "1rem"), ("font-weight:", "bold")]
+                            },
+                            {
+                                "selector":"th.row_heading",
+                                "props": [("background-color", "SteelBlue"), ("color", "white"),
+                                          ("border", "3px solid white"),
+                                          ("font-size", "1rem"), ("font-style", "italic")]
+                            },
+                            {
+                                "selector":"th.col_heading",
+                                "props": [("color", "white"),
+                                          ("border", "3px solid white"),
+                                          ("font-size", "1rem"), ("font-weight:", "bold")]
+                            }
+                        ])
+                voip_plan_table_style =  voip_plan_table_design.style.set_table_styles([
+                            {
+                                "selector":"thead",
+                                "props": [("background-color", "LightCoral"), ("color", "white"),
+                                          ("border", "3px solid white"),
+                                          ("font-size", "1rem"), ("font-weight:", "bold")]
+                            },
+                            {
+                                "selector":"th.row_heading",
+                                "props": [("background-color", "SteelBlue"), ("color", "white"),
+                                          ("border", "3px solid white"),
+                                          ("font-size", "1rem"), ("font-style", "italic")]
+                            },
+                            {
+                                "selector":"th.col_heading",
+                                "props": [("color", "white"),
+                                          ("border", "3px solid white"),
+                                          ("font-size", "1rem"), ("font-weight:", "bold")]
+                            }
+                        ])
+                w10_plan_table_style =  w10_plan_table_design.style.set_table_styles([
+                            {
+                                "selector":"thead",
+                                "props": [("background-color", "LightCoral"), ("color", "white"),
+                                          ("border", "3px solid white"),
+                                          ("font-size", "1rem"), ("font-weight:", "bold")]
+                            },
+                            {
+                                "selector":"th.row_heading",
+                                "props": [("background-color", "SteelBlue"), ("color", "white"),
+                                          ("border", "3px solid white"),
+                                          ("font-size", "1rem"), ("font-style", "italic")]
+                            },
+                            {
+                                "selector":"th.col_heading",
+                                "props": [("color", "white"),
+                                          ("border", "3px solid white"),
+                                          ("font-size", "1rem"), ("font-weight:", "bold")]
+                            }
+                        ])
+                addon_plan_table_style =  addon_plan_table_design.style.set_table_styles([
+                            {
+                                "selector":"thead",
+                                "props": [("background-color", "LightCoral"), ("color", "white"),
+                                          ("border", "3px solid white"),
+                                          ("font-size", "1rem"), ("font-weight:", "bold")]
+                            },
+                            {
+                                "selector":"th.row_heading",
+                                "props": [("background-color", "SteelBlue"), ("color", "white"),
+                                          ("border", "3px solid white"),
+                                          ("font-size", "1rem"), ("font-style", "italic")]
+                            },
+                            {
+                                "selector":"th.col_heading",
+                                "props": [("color", "white"),
+                                          ("border", "3px solid white"),
+                                          ("font-size", "1rem"), ("font-weight:", "bold")]
+                            }
+                        ])
+                aad_plan_table_style =  aad_plan_table_design.style.set_table_styles([
+                            {
+                                "selector":"thead",
+                                "props": [("background-color", "LightCoral"), ("color", "white"),
+                                          ("border", "3px solid white"),
+                                          ("font-size", "1rem"), ("font-weight:", "bold")]
+                            },
+                            {
+                                "selector":"th.row_heading",
+                                "props": [("background-color", "SteelBlue"), ("color", "white"),
+                                          ("border", "3px solid white"),
+                                          ("font-size", "1rem"), ("font-style", "italic")]
+                            },
+                            {
+                                "selector":"th.col_heading",
+                                "props": [("color", "white"),
+                                          ("border", "3px solid white"),
+                                          ("font-size", "1rem"), ("font-weight:", "bold")]
+                            }
+                        ])        
+                ##### Convert the Dataframe to a Streamlit Table #####        
+                col2.table(base_plan_table_style)
+                col2.table(ems_plan_table_style)
+                col2.table(mdo_plan_table_style)
+                col2.table(voip_plan_table_style)
+                col2.table(w10_plan_table_style)
+                col2.table(addon_plan_table_style)
+                col2.table(aad_plan_table_style)
 
 ##### Function for the EMS License Dropdown #####                
 def featuresProvidedBasedOnChosenEmsBundle():
@@ -556,5 +805,6 @@ featuresProvidedBasedOnChosenMdoBundle()
 featuresProvidedBasedOnChosenW10Bundle()
 featuresProvidedBasedOnChosenAadBundle()
 costPerUserCalculator()
+featuresProvidedBasedOnChosenBaseBundleColumn2()
 # security_eggplant_sizing_scale()
 
